ba766075b1e4819315fdd71d295e4b47a46f279e
EC2 Default User <ec2-user@ip-172-31-15-229.ap-northeast-1.compute.internal>
2024-09-28 22:16:20 +0000

    [Finish]TopPageLayout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'  #この記述正しいか？　
+  get '/books/new'
+  post '/books' => 'books#create' #この記述正しい？
+  get '/books' => 'books#index'  #この記述正しいか？　
   get 'books/show'
   get 'books/edit'
   get '/' => 'homes#top'
