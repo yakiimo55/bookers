13da86a11acc9891f49bdbfcee3eb4731a0e5495
EC2 Default User <ec2-user@ip-172-31-15-229.ap-northeast-1.compute.internal>
2024-10-02 02:48:26 +0000

    Error発生

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,16 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new  # @listでなくてよい
+    @book = Book.new  # @listではない
   end
 
   def create
     book = Book.new # newの後に(book_params)を記述するとエラー　なぜ？データを受け取り新規登録するためのインスタンス作成
     book.save  # 3. データをデータベースに保存するためのsaveメソッド実行
-    redirect_to book_path(book.id)  # 4. トップ画面へリダイレクト
+    redirect_to book_path(book.id)  
   end
 
   def index
+    @book = Book.all
   end
 
   def show
