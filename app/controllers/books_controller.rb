8a93500b54d1035334ffbdac0326e0ccc04d2675
EC2 Default User <ec2-user@ip-172-31-15-229.ap-northeast-1.compute.internal>
2024-10-07 06:49:38 +0000

    [Finish]validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,21 +26,16 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     @books = Book.all
-    if @book.save  # 3. データをデータベースに保存するためのsaveメソッド実行
-      flash[:success] = "done successfully"
-      redirect_to book_path(@book.id)
-    else
-      render 'edit'
-    end
+    
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:success] = "done successfully"
-      redirect_to book_path(params[:id])  #showアクションにredirect [:id]の意味
+      redirect_to book_path(@book.id)  #showアクションにredirect [:id]の意味
     else render 'edit'
-   end
+    end
   end
   
   def destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  # @ book = Book.find(params[: id])これを書いたらエラーになった
   validates :title, presence: true
   validates :body, presence: true
 
